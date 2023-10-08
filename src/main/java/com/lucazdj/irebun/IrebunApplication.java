a2a1e1cce928375e5fee3012bf0902ee69a5eec4
lucazdj <lucasdejesus.ldr@gmail.com>
2023-10-08 19:55:51 -0400

    ...

---
+++ src/main/java/com/lucazdj/irebun/IrebunApplication.java
@@ -0,0 +1,13 @@
+package com.lucazdj.irebun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrebunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrebunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucazdj/irebun/IrebunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucazdj.irebun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrebunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
